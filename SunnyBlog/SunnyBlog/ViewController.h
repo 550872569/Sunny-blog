6546612d9d3fb13201dce32d0b330034a9b4e0d3
YY <550872569@qq.com>
2018-05-24 22:05:10 +0800

    SunnyBlog

---
+++ SunnyBlog/SunnyBlog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SunnyBlog
+//
+//  Created by Yan on 2018/5/24.
+//  Copyright © 2018年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SunnyBlog/SunnyBlogTests/SunnyBlogTests.m
@@ -0,0 +1,39 @@
+//
+//  SunnyBlogTests.m
+//  SunnyBlogTests
+//
+//  Created by Yan on 2018/5/24.
+//  Copyright © 2018年 YY. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SunnyBlogTests : XCTestCase
+
+@end
+
+@implementation SunnyBlogTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
